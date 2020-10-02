5cb9bc72738303d1025de0f5649c74bc7a455db3
Klepchik <70842003+Klepchik@users.noreply.github.com>
2020-10-02 17:19:49 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (array && array.length > 0) {
+		let minimalElementValue = array[0];
+
+		for (let i = 0; i < array.length; i++) {
+			if (array[i] < minimalElementValue) {
+				minimalElementValue = array[i];
+			}
+		}
+
+		return minimalElementValue;
+	}
+
+	else {
+		return 0;
+	}
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array && array.length > 0) {
+		let maximumElementValue = array[0];
+
+		for (let i = 0; i < array.length; i++) {
+			if (array[i] > maximumElementValue) {
+				maximumElementValue = array[i];
+			}
+		}
+
+	return maximumElementValue;
+	}
+
+	else {
+		return 0;
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array && array.length > 0) {
+		let total = 0;
+
+	for (let i = 0; i < array.length; i++) {
+		total = total + array[i];
+	}
+
+	arithmeticalAverage = total / array.length;
+
+	return arithmeticalAverage;
+	}
+
+	else {
+		return 0;
+	}
 }
